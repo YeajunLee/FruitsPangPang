1de5912331995ccd14f87f94457f3c4b0430de40
boy0501 <boy0501@naver.com>
2022-05-02 15:26:26 +0900

    2022-05-02
    작업자 : 이수민
    작업내용 :
    Ai를 1개만 두었을때 생기는 버그 수정.
    
    overID는 0부터시작하는데 1에 예외를 두어서 2개이상두어야지 패킷들이 제대로 도착하는 문제 수정

--- FruitsPangPang/FruitsPangPang/Source/FruitsPangPang/Network.cpp
+++ FruitsPangPang/FruitsPangPang/Source/FruitsPangPang/Network.cpp
@@ -767,7 +767,7 @@ void Network::process_Aipacket(int client_id, unsigned char* p)
 		//for문으로 하지말고, Map 구조 Key-value 구조를 사용하여 최적화 할 것.
 		// ... 으나! 다른 버그가 터져서 결국 서버에서 판별하기로 결정...
 		bool escape = false;
-		if (PacketOwner->overID != 1) escape = true;
+		if (0 != PacketOwner->overID) break;
 		for (auto ai : mAiCharacter)
 		{
 			if (ai == nullptr) continue;
@@ -802,7 +802,7 @@ void Network::process_Aipacket(int client_id, unsigned char* p)
 		//if Ai Move, dump packet
 		//Ai가 움직이는 패킷은 버린다. 어차피 클라 내에서 움직이는거라 패킷으로 안움직여도 된다.
 		bool escape = false;
-		if (PacketOwner->overID != 1) escape = true;
+		if (0 != PacketOwner->overID) break;
 		for (auto ai : mAiCharacter)
 		{
 			if (ai == nullptr) continue;
@@ -933,7 +933,7 @@ void Network::process_Aipacket(int client_id, unsigned char* p)
 		int id = packet->id;
 
 		bool escape = false;
-		if (PacketOwner->overID != 1) escape = true;
+		if (0 != PacketOwner->overID) break;
 		for (auto ai : mAiCharacter)
 		{
 			if (ai == nullptr) continue;
@@ -966,7 +966,7 @@ void Network::process_Aipacket(int client_id, unsigned char* p)
 		sc_packet_spawnobj* packet = reinterpret_cast<sc_packet_spawnobj*>(p);
 		int other_id = packet->id;
 		bool escape = false;
-		if (PacketOwner->overID != 1) escape = true;
+		if (0 != PacketOwner->overID) break;
 		for (auto ai : mAiCharacter)
 		{
 			if (ai == nullptr) continue;
